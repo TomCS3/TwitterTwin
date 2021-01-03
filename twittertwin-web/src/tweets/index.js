7ab6df16b6848982bb6ccd34b97d7f25029988fc
TomCS3 <62783156+TomCS3@users.noreply.github.com>
2021-01-03 12:25:30 +0000

    Components Ordered

--- twittertwin-web/src/tweets/index.js
+++ twittertwin-web/src/tweets/index.js
@@ -1,8 +1,14 @@
-import {ActionBtn, Tweet, TweetsList, TweetsComponent} from './components'
+import {ActionBtn} from './buttons'
+
+import {Tweet} from './detail'
+import {TweetsComponent} from './components'
+import {TweetCreate} from './create'
+import {TweetsList} from './list'
 
 export {
     ActionBtn,
     Tweet,
-    TweetsList,
     TweetsComponent,
+    TweetCreate,
+    TweetsList,
 }
